f9105d80cdc510120d88dec8e9836845f86c3be2
firefingers21 <online+github@alexhuisman.ca>
2025-03-14 15:35:40 -0700

    Improve summary pattern matching

--- Workflow/lookup.sh
+++ Workflow/lookup.sh
@@ -5,39 +5,40 @@ output="# WHOIS ${1}"
 
 # Domain Information
 if [[ $domainInfo -eq 1 ]]; then
-	emptyCheck=$(awk '/(^Domain Name|^domain)/ && !seen[$0]++ { if ($0 ~ /\./) arr[0] = "* **Domain Name**: " tolower($NF) }
-	/^[Rr]egistrar:[^$]/ && !seen[$0]++ { $1=""; arr[1] = "* **Registrar**:" $0 }
-	/^Creation Date/ && !seen[$0]++ { arr[2] = "* **Creation Date**: " substr($3,0,10) }
-	/Expir.*Date:/ && !seen[$0]++ { arr[3] = "* **Expiry Date**: " substr($NF,0,10) }
-	/(^Updated Date|^[Ll]ast.?[Uu]pdate)/ && !seen[$0]++ { arr[4] = "* **Renewed Date**: " substr($NF,0,10) }
+	emptyCheck=$(awk 'BEGIN { dateRegex = "[0-9]{4}-[0-9]{2}-[0-9]{2}" }
+	/(^[[:blank:]]*Domain Name|^domain)/ && !seen[$0]++ { if ($0 ~ /\./) arr[0] = "* **Domain Name**: " tolower($NF) }
+	/^[[:blank:]]*[Rr]egistrar:[^$]/ && !seen[$0]++ { $1=""; arr[1] = "* **Registrar**:" $0 }
+	/^[[:blank:]]*Creation Date/ && !seen[$0]++ { start = match($0,dateRegex); arr[2] = "* **Creation Date**: " substr($0,start,10) }
+	/Expir.*Date:/ && !seen[$0]++ { start = match($0,dateRegex); arr[3] = "* **Expiry Date**: " substr($0,start,10) }
+	/(^[[:blank:]]*Updated Date|^[Ll]ast.?[Uu]pdate)/ && !seen[$0]++ { start = match($0,dateRegex); arr[4] = "* **Renewed Date**: " substr($0,start,10) }
 	END {
 		for (i = 0; i < length(arr); i++)
 			print arr[i]
 	}' <<< "${whois}")
 	[[ -n "${emptyCheck}" ]] && output+="\n\n### Domain Information\n${emptyCheck}"
 
-	emptyCheck=$(awk '/^Name Server/ && !seen[tolower($0)]++ { print "* **Name Server**:", $3 }' <<< "${whois}")
+	emptyCheck=$(awk '/^[[:blank:]]*Name Server/ && !seen[tolower($NF)]++ { print "* **Name Server**:", tolower($NF) }' <<< "${whois}")
 	[[ "${emptyCheck}" != "* **Name Server**: " ]] && output+="\n${emptyCheck}"
-	
-	emptyCheck=$(awk '/^Registrar Abuse Contact Email:[^$]/ && !seen[$0]++ { print "* **Registrar Abuse Contact Email**:", tolower($5) }' <<< "${whois}")
-	[[ -n "${emptyCheck}" ]] && output+="\n${emptyCheck}"
+
+	emptyCheck=$(awk '/^[[:blank:]]*Registrar Abuse Contact Email:[^$]/ && !seen[$5]++ { print "* **Registrar Abuse Contact Email**:", tolower($5) }' <<< "${whois}")
+	[[ "${emptyCheck}" != "* **Registrar Abuse Contact Email**: "  ]] && output+="\n${emptyCheck}"
 fi
 
 # Contact Information
 if [[ $registrantContact -eq 1 ]]; then
-	emptyCheck=$(awk '/^Registrant/ && !seen[$0]++ { if (toupper($0) !~ /REDACTED/ && $0 !~ /(:$|: $)/ && $0 !~ /Email[^@]*$/) print "*", $0 }' <<< "${whois}")
+	emptyCheck=$(awk '/^Registrant/ && !seen[$0]++ { if (toupper($0) !~ /(REDACTED|GDPR.MASK)/ && $0 !~ /(:$|: $)/ && $0 !~ /Email[^@]*$/) print "*", $0 }' <<< "${whois}")
 	[[ -n "${emptyCheck}" ]] && output+="\n\n### Registrant Contact\n${emptyCheck}"
 fi
 if [[ $adminContact -eq 1 ]]; then
-	emptyCheck=$(awk '/^Admin/ && !seen[$0]++ { if (toupper($0) !~ /REDACTED/ && $0 !~ /(:$|: $)/ && $0 !~ /Email[^@]*$/) print "*", $0 }' <<< "${whois}")
+	emptyCheck=$(awk '/^Admin/ && !seen[$0]++ { if (toupper($0) !~ /(REDACTED|GDPR.MASK)/ && $0 !~ /(:$|: $)/ && $0 !~ /Email[^@]*$/) print "*", $0 }' <<< "${whois}")
 	[[ -n "${emptyCheck}" ]] && output+="\n\n### Administrative Contact\n${emptyCheck}"
 fi
 if [[ $techContact -eq 1 ]]; then
-	emptyCheck=$(awk '/^Tech/ && !seen[$0]++ { if (toupper($0) !~ /REDACTED/ && $0 !~ /(:$|: $)/ && $0 !~ /Email[^@]*$/) print "*", $0 }' <<< "${whois}")
+	emptyCheck=$(awk '/^Tech/ && !seen[$0]++ { if (toupper($0) !~ /(REDACTED|GDPR.MASK)/ && $0 !~ /(:$|: $)/ && $0 !~ /Email[^@]*$/) print "*", $0 }' <<< "${whois}")
 	[[ -n "${emptyCheck}" ]] && output+="\n\n### Technical Contact\n${emptyCheck}"
 fi
 if [[ $billingContact -eq 1 ]]; then
-	emptyCheck=$(awk '/^Billing/ && !seen[$0]++ { if (toupper($0) !~ /REDACTED/ && $0 !~ /(:$|: $)/ && $0 !~ /Email[^@]*$/) print "*", $0 }' <<< "${whois}")
+	emptyCheck=$(awk '/^Billing/ && !seen[$0]++ { if (toupper($0) !~ /(REDACTED|GDPR.MASK)/ && $0 !~ /(:$|: $)/ && $0 !~ /Email[^@]*$/) print "*", $0 }' <<< "${whois}")
 	[[ -n "${emptyCheck}" ]] && output+="\n\n### Billing Contact\n${emptyCheck}"
 fi
 
