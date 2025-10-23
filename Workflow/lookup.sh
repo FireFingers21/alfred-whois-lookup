bce12c424e60155dfdcc90bf58e5b9b0fcf0cd95
firefingers21 <online+github@alexhuisman.ca>
2025-10-22 17:53:22 -0700

    Optimize code for fetching/parsing WHOIS data

--- Workflow/lookup.sh
+++ Workflow/lookup.sh
@@ -1,26 +1,8 @@
 #!/bin/zsh --no-rcs
 
-if [[ "${forceReload}" -ne 1 ]] && [[ -f "${whois_file}" ]] && [[ "$(date -r "${whois_file}" +%s)" -gt "$(date -v -"359"M +%s)" ]]; then
-    # Lookup WHOIS cache file
-    readonly whois=$(cat "${whois_file}")
-else
-    # Lookup WHOIS server
-    [[ "${forceReload}" -eq 1 ]] && 1=$(echo "${1:6}" | head -n 1)
-    readonly whois=$(whois "${1}" | tr -d '\r')
-    mkdir -p "${alfred_workflow_cache}"
-    # Only write to cache if valid entry
-    if [[ -z "$(awk '/(^\% This query returned 0 objects\.|^\% Error: Invalid query|^Domain not found|^No match for domain)/' <<< "${whois}")" ]]; then
-        echo "${whois}" > "${whois_file}"
-    fi
-fi
-# Skip formatting if viewing as text file
-[[ "${fileView}" -eq 1 ]] && open "${whois_file}" && exit
-
-output="# WHOIS ${1}"
-
 # Domain Information
-if [[ $domainInfo -eq 1 ]]; then
-	emptyCheck=$(awk 'BEGIN { dateRegex = "[0-9]{4}-[0-9]{2}-[0-9]{2}"; nsArr = 0; abuseArr = 0 }
+function domainSummary {
+	awk 'BEGIN { dateRegex = "[0-9]{4}-[0-9]{2}-[0-9]{2}"; nsArr = 0; abuseArr = 0 }
 	/(^[[:blank:]]*Domain [Nn]ame|^domain)/ && !seen[$0]++ { if ($0 ~ /\./) arr[0] = "* **Domain Name**: " tolower($NF) }
 	/^[[:blank:]]*[Rr]egistrar:[^$]/ && !seen[$0]++ { $1=""; arr[1] = "* **Registrar**:" $0 }
 	/^[[:blank:]]*Creation Date/ && !seen[$0]++ { start = match($0,dateRegex); arr[2] = "* **Creation Date**: " substr($0,start,10) }
@@ -34,10 +16,9 @@ if [[ $domainInfo -eq 1 ]]; then
 		for (i = 0; i < length(ns); i++)
 			print ns[i]
 		for (i = 0; i < length(abuseArr); i++)
-			print abuse[i]
-	}' <<< "${whois}")
-	[[ -n "${emptyCheck}" ]] && output+="\n\n### Domain Information\n${emptyCheck}"
-fi
+			if (abuseArr > 0) print abuse[i]
+	}' <<< "${whois}" | xargs -0 printf "\\n\\n### Domain Information\\n%s"
+}
 
 # Contact Information
 blacklist=("Domains By Proxy, LLC" \
@@ -57,30 +38,28 @@ function contactSummary {
         [[ "${blacklist[@]}" == *"${contactOrg}"* ]] && return
     fi
     # Fetch contact information
-    awk -v pattern="${1}" '$0 ~ pattern && !seen[$0]++ { if (toupper($0) !~ /(REDACTED|GDPR.MASK)/ && $0 !~ /(:$|: $)/ && $0 !~ /Email[^@]*$/) print "*", $0 }' <<< "${whois}"
+    awk -v pattern="^${1}" '$0 ~ pattern && !seen[$0]++ { if (toupper($0) !~ /(REDACTED|GDPR.MASK)/ && $0 !~ /(:$|: $)/ && $0 !~ /Email[^@]*$/) print "*", $0 }' <<< "${whois}" | \
+    xargs -0 printf "\\n### ${1} Contact\\n%s"
 }
-if [[ $registrantContact -eq 1 ]]; then
-	emptyCheck=$(contactSummary "^Registrant")
-	[[ -n "${emptyCheck}" ]] && output+="\n\n### Registrant Contact\n${emptyCheck}"
-fi
-if [[ $adminContact -eq 1 ]]; then
-	emptyCheck=$(contactSummary "^Admin")
-	[[ -n "${emptyCheck}" ]] && output+="\n\n### Administrative Contact\n${emptyCheck}"
-fi
-if [[ $techContact -eq 1 ]]; then
-	emptyCheck=$(contactSummary "^Tech")
-	[[ -n "${emptyCheck}" ]] && output+="\n\n### Technical Contact\n${emptyCheck}"
-fi
-if [[ $billingContact -eq 1 ]]; then
-	emptyCheck=$(contactSummary "^Billing")
-	[[ -n "${emptyCheck}" ]] && output+="\n\n### Billing Contact\n${emptyCheck}"
+
+# Fetch WHOIS data
+if [[ "${forceReload}" -ne 1 && -f "${whois_file}" ]] && [[ "$(date -r "${whois_file}" +%s)" -gt "$(date -v -"359"M +%s)" ]]; then
+    readonly whois=$(< "${whois_file}")
+else
+    readonly whois=$(whois "${domain}" | tr -d '\r')
+    # Only write to cache if valid response
+    mkdir -p "${alfred_workflow_cache}"
+    grep -q -E "^(\% This query returned 0 objects\.|\% Error: Invalid query|Domain not found|No match for domain)" <<< "${whois}" || echo "${whois}" > "${whois_file}"
 fi
 
-# Raw WHOIS Data
-output+=$(echo "\n\n## Raw WHOIS Data\n
-\`\`\`
-${whois}
-\`\`\`")
+# Skip formatting if viewing as text file
+[[ "${fileView}" -eq 1 ]] && open "${whois_file}" && exit
 
-# Output Formatted WHOIS Data to Text View
-echo "${output}"
+# Output formatted whois data to Text View
+echo -n "# WHOIS ${domain}"
+[[ "${domainInfo}" -eq 1 ]] && domainSummary
+[[ "${registrantContact}" -eq 1 ]] && contactSummary "Registrant"
+[[ "${adminContact}" -eq 1 ]] && contactSummary "Admin"
+[[ "${techContact}" -eq 1 ]] && contactSummary "Tech"
+[[ "${billingContact}" -eq 1 ]] && contactSummary "Billing"
+echo "\n## Raw WHOIS Data\n\`\`\`\n${whois}\n\`\`\`"
